1dcf17c5aace823f20d6f76dc6612015e625b33a
Yubaraj Magar <yubaraj.100mgr@gmail.com>
2022-08-04 13:36:07 +1000

    all packages is installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button>Submit</Button>
+    </div>
+  );
 };
 
 export default App;
